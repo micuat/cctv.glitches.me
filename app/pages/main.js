adc0d2f3c0795ae0ba57a58850bf79d40554c104
micuat <micuat@gmail.com>
2023-11-02 12:34:28 +0100

    desc

--- app/pages/main.js
+++ app/pages/main.js
@@ -1,10 +1,35 @@
 import html from "choo/html";
+import { css } from "@emotion/css";
+
+const mainCss = css`
+h1 {
+  font-size: 1.5rem;
+  margin: 0.5rem 0;
+}
+div {
+  margin: 0.95rem 0;
+}
+button {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+`;
 
 // export module
 export default function(state, emit) {
 
   return html`
-  <div>
+  <div class=${ mainCss }>
+    <h1>cctv.glitches.me</h1>
+    <div>
+    </div>
+    <div>
+      Guard will be back momentarily.
+    </div>
+    <div>
+      Meanwhile, you can follow me on https://dawatcher.tumblr.com
+    </div>
     <button onclick="${ open }">open</button>
   </div>
   `;
